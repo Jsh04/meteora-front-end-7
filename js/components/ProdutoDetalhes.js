431890120524ab4646266bc4be8d631c1cd603b1
José Silvio <josesilvio.bs@gmail.com>
2023-08-21 20:54:12 -0300

    Começando a implementação do Produtos Detalhes

---
+++ js/components/ProdutoDetalhes.js
@@ -0,0 +1,16 @@
+export default {
+
+    template: /*html*/ `
+    <article class="produto-detalhes">
+        <div class="produto-detalhes__cabecalho">
+            <img class="produto-detalhes__cabecalho-icon">
+            <h2 class="produto-detalhes__cabecalho-texto">Confira detalhes sobre o produto</h2>
+            <p class="produto-detalhes__cabecalho">X</p>
+        </div>
+        
+    
+    </article>
+    
+    
+    `
+}

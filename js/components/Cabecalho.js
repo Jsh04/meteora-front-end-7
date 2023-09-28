7b591af37a9a5a70e7048271c4cbd26b6939015d
José Silvio <josesilvio.bs@gmail.com>
2023-09-27 21:07:21 -0300

    Começando a responsividade do Mobile

--- js/components/Cabecalho.js
+++ js/components/Cabecalho.js
@@ -10,6 +10,11 @@ export default {
                 <li class="cabecalho__item">Promoções</li>
             </ul>
         </div>
+        <div class="cabecalho__btn-mobile">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="15" viewBox="0 0 18 15" fill="none">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M0 0.571289H18V2.82129H0V0.571289ZM0 6.19629H18V8.44629H0V6.19629ZM0 11.8213H18V14.0713H0V11.8213Z" fill="#DAFF01"/>
+            </svg>
+        </div>
         <div class="cabecalho__section-busca">
             <input type="text" v-model="filtro" id="search" name="search" placeholder="Digite um produto" class="cabecalho__input-busca">
             <button class="cabecalho__button-busca" @click="enviarFiltro()" type="submit">Buscar</button>
@@ -20,15 +25,22 @@ export default {
     name: "Cabecalho",
     data() {
         return {
-            filtro: ''
+            filtro: '',
+            isWidthMobilt: false
         }
     },
     methods: {
         enviarFiltro(){
             console.log("Apertou no botão")
             console.log(this.filtro)
+        },
+        redimensionamentoTelaMobile(){
+
         }
     },
+    mounted(){
+        console.log(window.innerWidth);
+    }
 }
 
 
